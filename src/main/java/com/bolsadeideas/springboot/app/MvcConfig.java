934f9850a498e8fc0c0b71bacac7f3aa765c87be
Cristian Ramirez <c.ramirez0391@gmail.com>
2018-09-02 16:12:07 -0500

    First commit

---
+++ src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -0,0 +1,21 @@
+package com.bolsadeideas.springboot.app;
+
+import java.nio.file.Paths;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter{
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		super.addResourceHandlers(registry);
+		String resourcePath = Paths.get("uploads").toAbsolutePath().toUri().toString();
+		
+		registry.addResourceHandler("/uploads/**")
+		.addResourceLocations(resourcePath);
+	}
+	 
+}
