75251f254537e9645bf637af1647357f1c9fd5e5
Cristian Ramirez <c.ramirez0391@gmail.com>
2018-09-02 16:47:42 -0500

    Se agrega eliminar y editar foto

--- src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -1,5 +1,6 @@
 package com.bolsadeideas.springboot.app.controllers;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
@@ -45,10 +46,11 @@ public class ClienteController {
 	private IClienteService clienteService;
 
 	private final Logger log = LoggerFactory.getLogger(getClass());
+	private final String UPLOAD_FOLDER = "uploads";
 
 	@GetMapping(value = "/uploads/{filename:.+}")
 	public ResponseEntity<Resource> verFoto(@PathVariable String filename) {
-		Path pathFoto = Paths.get("uploads").resolve(filename).toAbsolutePath();
+		Path pathFoto = Paths.get(UPLOAD_FOLDER).resolve(filename).toAbsolutePath();
 		log.info("pathfoto : " + pathFoto);
 		Resource resource = null;
 		try {
@@ -124,6 +126,14 @@ public class ClienteController {
 			return "form";
 		}
 		if (!foto.isEmpty()) {
+			if (cliente.getId() != null && cliente.getId() != 0 && cliente.getFoto() != null
+					&& !cliente.getFoto().isEmpty()) {
+				Path rootPath = Paths.get(UPLOAD_FOLDER).resolve(cliente.getFoto()).toAbsolutePath();
+				File archivo = rootPath.toFile();
+				if (archivo.exists() && archivo.canRead()) {
+					archivo.delete();
+				}
+			}
 
 			String uniqueFileName = UUID.randomUUID().toString() + "_" + foto.getOriginalFilename();
 			Path rootPath = Paths.get("uploads").resolve(foto.getOriginalFilename());
@@ -151,8 +161,17 @@ public class ClienteController {
 	public String eliminar(@PathVariable(value = "id") Long id, RedirectAttributes flash) {
 
 		if (id > 0) {
+			Cliente cliente = clienteService.findOne(id);
+
 			clienteService.delete(id);
 			flash.addFlashAttribute("success", "Cliente eliminado con éxito!");
+			Path rootPath = Paths.get(UPLOAD_FOLDER).resolve(cliente.getFoto()).toAbsolutePath();
+			File archivo = rootPath.toFile();
+			if (archivo.exists() && archivo.canRead()) {
+				if (archivo.delete()) {
+					flash.addFlashAttribute("info", "Foto eliminada: " + cliente.getFoto());
+				}
+			}
 		}
 		return "redirect:/listar";
 	}
