2cdabf51bb29f6b9b1d3520da60d67bde8fabc27
Cristian Ramirez <c.ramirez0391@gmail.com>
2018-09-02 19:05:54 -0500

    Se agrega la eliminacion y creacion de folder upload

--- src/main/java/com/bolsadeideas/springboot/app/models/service/IUploadFileService.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/service/IUploadFileService.java
@@ -10,4 +10,6 @@ public interface IUploadFileService {
 	public Resource load(String filename)throws MalformedURLException;
 	public String copy(MultipartFile file)throws IOException ;
 	public boolean delete(String filename);
+	public void deleteAll();
+	public void init()throws IOException ;
 }
